3a487c446bf7b7bdb429224251bb52e5a9c7a51c
Antonia Jovanovska <antonia@alpineinteractive.co.uk>
2015-04-06 16:19:14 +0100

    Create FlowmasterApplication.cs

---
+++ App_Code/FlowmasterApplication.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using umbraco.businesslogic;
+using umbraco.interfaces;
+
+namespace Flowmaster
+{
+    [Application("flowmaster", "Flowmaster", "icon-nodes")]
+    public class DocCreatorApplication : IApplication { }
+}
